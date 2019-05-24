a0846c7548ab71112a3c462c51ff9c51d6bc3aa0
Bai-BaiBai <877049473@qq.com>
2019-05-24 10:48:11 +0800

    蚂蚁信息及动作

--- src/AntGroup/Ant.java
+++ src/AntGroup/Ant.java
@@ -7,23 +7,24 @@ import java.util.Random;
  */
 public class Ant {
 
-    public static final int ATTRACTIVE_CONSTANT = 100;//吸引力放大倍数
-    public static final double PHEROMONE_CONSTANT = 10;//信息素更新的放大倍数
+    public static final int ATTRACTIVE_CONSTANT = 20;//吸引力放大倍数
 
-    public City city;
+    public City city;//所属城市群
     public boolean[] visitedFlag;//访问标志数组,false为可访问，true为已访问过
     public int[] path;//走过的路径
+    public int startCity;//蚂蚁的出发地
 
-    public Ant(City cities){
+    public Ant(City cities, int startCity){
         this.city = cities;
+        this.startCity = startCity;
         getInitVisitedFlag();//初始化访问标志数组
         initPath();
     }
 
     //初始化的访问标志数组，初始全为false
     public boolean[] getInitVisitedFlag(){
-        visitedFlag = new boolean[city.CITYSIZE];
-        for (int i = 0; i < city.CITYSIZE; i++) {
+        visitedFlag = new boolean[City.CITYSIZE];
+        for (int i = 0; i < City.CITYSIZE; i++) {
             visitedFlag[i] = false;
         }
         return visitedFlag;
@@ -32,7 +33,7 @@ public class Ant {
     //初始化走过的路径
     public void initPath(){
 
-        path = new int[city.CITYSIZE];
+        path = new int[City.CITYSIZE];
     }
 
     //获取走过的路径
@@ -41,14 +42,15 @@ public class Ant {
     }
 
     //传入蚂蚁初始的位置，方法结束后Path数组存储这次行走的路径
-    public void travel(int startCity){
-        for (int i = 0; i < city.CITYSIZE; i++) {//i代表下一步走的是第i步
+    public void travel(){
+        int currentCity = startCity;
+        for (int i = 0; i < City.CITYSIZE; i++) {//i代表下一步走的是第i步
 //            System.out.println("第" + i + "步" + startCity);
-            int res = moveToNextCity(startCity, i);
-            while (res == -1 && i != city.CITYSIZE -1){
-                res = moveToNextCity(startCity, i);
+            int res = moveToNextCity(currentCity, i);
+            while (res == -1 && i != City.CITYSIZE -1){
+                res = moveToNextCity(currentCity, i);
             }
-            startCity = res;
+            currentCity = res;
         };
     }
 
@@ -60,7 +62,7 @@ public class Ant {
         //选择下一步城市,更新信息素矩阵
         int res = chooseNextCity(currentCity);
         if (res != -1) {
-            updatePheromone(currentCity, res);
+            city.updatePheromone(currentCity, res);
         }
         return res;
     }
@@ -68,17 +70,20 @@ public class Ant {
     //选择下一步要走的城市 :有可能返回-1
     public int chooseNextCity(int currentCity){
 
-        double[] attractive = new double[city.CITYSIZE];//存储剩余可走城市对蚂蚁的吸引力
+        double[] attractive = new double[City.CITYSIZE];//存储剩余可走城市对蚂蚁的吸引力
 
         double sumAttractive = 0.00;
-        for (int i = 0; i < city.CITYSIZE; i++) {
+        for (int i = 0; i < City.CITYSIZE; i++) {
             if (!visitedFlag[i]){//如果此城市没有被访问过
-                attractive[i] = ATTRACTIVE_CONSTANT * 1/city.distance[currentCity][i] * city.pheromone[currentCity][i];
+                attractive[i] = ATTRACTIVE_CONSTANT /Math.pow(city.distance[currentCity][i],2) * city.pheromone[currentCity][i];
                 sumAttractive += attractive[i];
             }else {
                 attractive[i] = 0;//如果访问过，吸引力为0
             }
         }
+
+        //输出吸引力
+//        outPutAttractive(attractive, currentCity);
 
         //轮盘赌算法找到吸引力最大的城市并判断合法性返回
         int nextCity = roulette(sumAttractive, attractive);
@@ -89,17 +94,17 @@ public class Ant {
     //轮盘赌算法，传入各城市的当前吸引力和 吸引力总和,返回选出的城市代号
     public int roulette(double sumAttractive, double[] attractive){
 
-        double[] probability = new double[city.CITYSIZE];//存储走i城市对应的总概率
+        double[] probability = new double[City.CITYSIZE];//存储走i城市对应的总概率
 
         probability[0] = attractive[0] / sumAttractive;
-        for (int i = 1; i < city.CITYSIZE; i++) {
-            probability[i] =probability[i-1] + attractive[i] / sumAttractive;
+        for (int i = 1; i < City.CITYSIZE; i++) {
+            probability[i] = probability[i-1] + attractive[i] / sumAttractive;
         }
 
         Random random = new Random();
-        double ran = random.nextDouble();//随机数
+        double ran = random.nextDouble();//生成随机数，范围[0..1)
 
-        for (int i = 0; i < city.CITYSIZE; i++) {
+        for (int i = 0; i < City.CITYSIZE; i++) {
             if (ran < probability[i]){
                 return i;
             }
@@ -108,10 +113,23 @@ public class Ant {
         return -1;
     }
 
-    //更新信息素矩阵:每走一步更新一步
-    public void updatePheromone(int currentCity, int nextCity){
-        double d = city.pheromone[currentCity][nextCity];
-        city.pheromone[currentCity][nextCity] = city.pheromone[nextCity][currentCity] =
-                d + PHEROMONE_CONSTANT/city.distance[currentCity][nextCity];
+    public int getPathDistance(){
+        int sum = 0;
+        for (int i = 1; i < City.CITYSIZE; i++) {
+            sum += city.distance[path[i-1]][path[i]];
+        }
+
+        return sum;
+    }
+
+    //输出在currentCity时，其它城市的吸引力
+    //需要在 chooseNextCity 方法中调用
+    public void outPutAttractive(double[] attractive, int currentCity){
+        StringBuilder res = new StringBuilder();
+        res.append("当前所在城市为：" + currentCity + " 其余城市的吸引力为：");
+        for (int i = 0; i < attractive.length; i++) {
+            res.append(attractive[i] + " ");
+        }
+        System.out.println(res.toString());
     }
 }
